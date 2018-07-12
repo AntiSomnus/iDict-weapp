5a5fc25b9847490d730c7d834029a87c1bbe02aa
xytao <850804004@qq.com>
2018-07-12 04:58:13 -0700

    modify app.py for article list restful

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_restful import Resource, Api, reqparse, inputs
 from google.protobuf.json_format import MessageToJson, MessageToDict
 
 import WordParsing
+from ArticleParsing import get_article_list
 from Dict.stardict import DictCsv, LemmaDB
 import requests
 
@@ -12,16 +13,24 @@ app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 api = Api(app)
 
-parser = reqparse.RequestParser()
-parser.add_argument('word', type=str, help='The word for query', required=True)
-parser.add_argument('stem', type=inputs.boolean, help='whether get stem', default=True)
-parser.add_argument('json', type=inputs.boolean, help='whether get json', default=False)
-parser.add_argument('indent', type=int, help='json incident', default=4)
+parser_word = reqparse.RequestParser()
+parser_word.add_argument('word', type=str, help='The word for query', required=True)
+parser_word.add_argument('stem', type=inputs.boolean, help='whether get stem', default=True)
+parser_word.add_argument('json', type=inputs.boolean, help='whether get json', default=False)
+parser_word.add_argument('indent', type=int, help='json incident', default=4)
+
+parser_article_list = reqparse.RequestParser()
+parser_article_list.add_argument('page', type=int, help='page_number', default=1)
+parser_article_list.add_argument('count', type=int, help='count number', default=10)
+parser_article_list.add_argument('source', type=str, help='source plain name', default=None)
+parser_article_list.add_argument('json', type=inputs.boolean, help='whether get json', default=False)
+parser_article_list.add_argument('indent', type=int, help='json incident', default=4)
 
 
 class Word(Resource):
+
     def get(self):
-        args = parser.parse_args()
+        args = parser_word.parse_args()
 
         word = args['word']
         is_stem = args['stem']
@@ -29,14 +38,43 @@ class Word(Resource):
         indent = args['indent']
         word_processing = WordParsing.WordProcess(word, is_stem)
         if is_json:
+            print(MessageToDict(word_processing.word_detail))
             return Response(
-                json.dumps(MessageToDict(word_processing.word_detail), indent=indent, ensure_ascii=False).encode(
+                json.dumps(MessageToDict(word_processing.word_detail), indent=indent,
+                           ensure_ascii=False,
+                           sort_keys=True
+                           ).encode(
                     'utf-8').decode(),
                 content_type="application/json")
         return Response(word_processing.word_detail.SerializeToString(), mimetype='application/x-protobuf')
 
 
-api.add_resource(Word, '/')
+class ArticleList(Resource):
 
+    def get(self):
+        args = parser_article_list.parse_args()
+        page = args['page']
+        count = args['count']
+        source_title = args['source']
+
+        is_json = args['json']
+        indent = args['indent']
+
+        article_list = get_article_list(page=page, count=count, source_title=source_title)
+
+        if is_json:
+            print(MessageToDict(article_list))
+            return Response(
+                json.dumps(MessageToDict(article_list), indent=indent,
+                           ensure_ascii=False,
+                           sort_keys=True
+                           ).encode(
+                    'utf-8').decode(),
+                content_type="application/json")
+        return Response(article_list.SerializeToString(), mimetype='application/x-protobuf')
+
+
+api.add_resource(Word, '/')
+api.add_resource(ArticleList, '/articleList')
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
