f5fd2717ec4c71e8ac2336abd987caf79cb72208
KIRI-Z <kiriz_@outlook.com>
2018-07-16 20:29:49 +0800

    untested: restructure of data stream

--- app/word/sql.py
+++ app/word/sql.py
@@ -1,162 +1,140 @@
 import json
-import xml.etree.cElementTree as ET
+# import xml.etree.cElementTree as ET
 
-import requests
+# import requests
 
-
-class SelectSQL(object):
+class OperateDB(object):
     def __init__(self, conn):
         self.conn = conn
-        self.key = ''
-        self.mode = ''
-        self.count = 1
-        self.fields = ('id', 'word', 'sw', 'phonetic', 'definition',
-                       'translation', 'pos', 'collins', 'oxford', 'tag',
-                       'bnc', 'frq', 'base', 'exchange', 'oxford_detail',
-                       'net_detail', 'audio')
-        self.list_sql = ('SELECT word, translation, tag '
-                         'FROM {table} '
-                         'WHERE word LIKE \'{key}%%\' '
-                         'ORDER BY base '
-                         'LIMIT {count}')
-        self.detail_sql = ('SELECT * '
-                           'FROM {table} '
-                           'WHERE word=\'{key}\' ')
+        self.table = ('word_mini', 'word_slim', 'word_entire')
 
-    def select(self, key, **kwargs):
-        if 'mode' not in kwargs:
+
+    def select_list(self, word, **kwargs):
+        word = word.replace('\'', '\'\'').replace('%','%%')
+        if 'count' in kwargs:
+            count = kwargs['count']
+        else:
+            count = 10
+        fields = ['word', 'chn_def']
+        optional_fields = {'pron': 'uk_pron, us_pron',
+                           'eng': 'eng_def',
+                           'tag': 'tag'}
+        for field in optional_fields.keys():
+            if field in kwargs and kwargs[field] == True:
+                fields.append(optional_fields[field])
+        fields = ' '.join(fields)
+
+        list_sql = ('SELECT {fields} '
+                    'FROM {table} '
+                    'WHERE word LIKE \'{word}%%\' '
+                    'ORDER BY base '
+                    'LIMIT {count}').format(fields=fields, word=word)
+        data = self.get_list_data(list_sql, count)
+
+        if len(data) == 0:
             return {'status': False}
-        self.key = key.replace('\'', '\'\'').replace('%','%%')
-        self.mode = kwargs['mode']
-        if self.mode == 'list':
-            self.count = kwargs['count']
+        result = {'status': True, 'data': []}
+        for item in data:
+            d = {}
+            d['word'] = item[0]
+            d['chn_def'] = item[1] if item[1] else ''
+            if item[2]:
+                d['uk_pron'] = item[2]
+            if item[3]:
+                d['us_pron'] = item[3]
+            if item[4]:
+                d['eng_def'] = item[4]
+            if item[5]:
+                d['tag'] = item[5]
+            result['data'].append(d)
+        return result
 
+    def get_list_data(self, list_sql, count):
         data = []
-        result = self.select_mini()
-        if result['status']:
-            data.extend(result['data'])
-            if self.count == 0:
-                return self.tuple2dict(data)
-        result = self.select_slim()
-        if result['status']:
-            data.extend(result['data'])
-            if self.count == 0:
-                return self.tuple2dict(data)
-        result = self.select_entire()
-        if result['status']:
-            data.extend(result['data'])
-            if self.count == 0:
-                return self.tuple2dict(data)
-        if len(data) > 0:
-            return self.tuple2dict(data)
-        return {'status': False}
+        for t in self.table:
+            sql = list_sql.format(table=t, count=count)
+            result = self.conn.execute(sql).fetchall()
+            if result != []:
+                data.extend(result)
+                count -= len(data)
+            if count == 0:
+                break
+        return data
 
-    def select_mini(self):
-        if self.mode == 'list':
-            sql = self.list_sql.format(
-                table='word_mini', key=self.key, count=self.count)
-            data = self.conn.execute(sql).fetchall()
-        if self.mode == 'detail':
-            sql = self.detail_sql.format(
-                table='word_mini', key=self.key)
-            data = self.conn.execute(sql).fetchall()
-        if data == []:
+    def select_detail(self, word):
+        word = word.replace('\'', '\'\'').replace('%','%%')
+        fields = ['collins', 'bnc', 'frq', 'oxford_detail', 'net_detail']
+        fields = ' '.join(fields)
+
+        detail_sql = ('SELECT {fields} '
+                      'FROM {table} '
+                      'WHERE word =\'{word}%%\'').format(fields=fields, word=word)
+        data = self.get_detail_data(detail_sql)
+        lemma = self.find_lemma(word)
+
+        if len(data) == 0:
             return {'status': False}
-        else:
-            self.count -= len(data)
-            return {'status': True, 'data': data}
+        d = {'lemma': lemma}
+        d['collins'] = data[0]
+        d['bnc'] = data[1]
+        d['frq'] = data[2]
+        d['oxford_detail'] = data[3]
+        d['net_detail'] = data[4]
+        result = {'status': True, 'data': d}
+        return result
 
-    def select_slim(self):
-        if self.mode == 'list':
-            sql = self.list_sql.format(
-                table='word_slim', key=self.key, count=self.count)
-            data = self.conn.execute(sql).fetchall()
-        if self.mode == 'detail':
-            sql = self.detail_sql.format(
-                table='word_slim', key=self.key)
-            data = self.conn.execute(sql).fetchall()
-        if data == []:
-            return {'status': False}
-        else:
-            self.count -= len(data)
-            return {'status': True, 'data': data}
+    def get_detail_data(self, detail_sql):
+        for t in self.table:
+            sql = detail_sql.format(table=t)
+            result = self.conn.execute(sql).fetchone()
+            if result is not None:
+                data = result
+                break
+        return data
 
-    def select_entire(self):
-        if self.mode == 'list':
-            sql = self.list_sql.format(
-                table='word_entire', key=self.key, count=self.count)
-            data = self.conn.execute(sql).fetchall()
-        if self.mode == 'detail':
-            sql = self.detail_sql.format(
-                table='word_entire', key=self.key)
-            data = self.conn.execute(sql).fetchall()
-        if data == []:
-            return {'status': False}
-        else:
-            return {'status': True, 'data': data}
+    def find_lemma(self, word):
+        sql = ('SELECT lemma, type '
+               'FROM word_lemma '
+               'WHERE word={word}').format(word=word)
+        data = self.conn.execute(sql).fetchone()
+        if data is not None:
+            lemma = {'word': data[0], 'relation':data[1]}
+        return lemma
 
-    def tuple2dict(self, data):
-        if self.mode == 'list':
-            words = {'status': True, 'data': []}
-            data = [item for item in data]
-            for item in data:
-                if item[1]:
-                    d = {'word': item[0],
-                         'translation': item[1],
-                         'tag': item[2]}
-                else:
-                    d = {'word': item[0],
-                         'translation': '',
-                         'tag': item[2]}
-                words['data'].append(d)
-            return words
-        if self.mode == 'detail':
-            word = {'status': True}
-            print(data)
-            data = [item for item in data[0]]
-            for i in range(len(self.fields)):
-                field = self.fields[i]
-                if field in ('definition', 'translation'):
-                    if data[i]:
-                        data[i] = data[i].replace('\\n', '\n')
-                word[field] = data[i]
-            return word
+# class UpdateDetail(object):
+#     def __init__(self, conn):
+#         self.conn = conn
+#         self.key = ''
 
+#     def fetch_from_iciba(self, key):
+#         self.key = key
+#         params = {'w': self.key, 'key': '341DEFE6E5CA504E62A567082590D0BD'}
+#         xml_bytes = requests.get(
+#             'http://dict-co.iciba.com/api/dictionary.php', params=params).content
+#         return self.parsing_from_xml(xml_bytes)
 
-class UpdateDetail(object):
-    def __init__(self, conn):
-        self.conn = conn
-        self.key = ''
-
-    def fetch_from_iciba(self, key):
-        self.key = key
-        params = {'w': self.key, 'key': '341DEFE6E5CA504E62A567082590D0BD'}
-        xml_bytes = requests.get(
-            'http://dict-co.iciba.com/api/dictionary.php', params=params).content
-        return self.parsing_from_xml(xml_bytes)
-
-    def parsing_from_xml(self, xml_bytes):
-        root = ET.fromstring(xml_bytes)
-        sentence_list = []
-        for sent in root.findall('sent'):
-            eng, chn = sent.getchildren()
-            eng = eng.text.strip()
-            chn = chn.text.strip()
-            sentence_list.append((eng, chn))
-        sentence = ['\n'.join([eng.replace('\'', '\'\''),
-                               chn.replace('\'', '\'\'')])
-                    for eng, chn in sentence_list]
-        sentence = '\r\n'.join(sentence)
-        sql = ('UPDATE word_mini '
-               'SET net_detail=\'{sentence}\' '
-               'WHERE word=\'{key}\'').format(sentence=sentence, key=self.key)
-        self.conn.execute(sql)
-        sql = ('UPDATE word_slim '
-               'SET net_detail=\'{sentence}\' '
-               'WHERE word=\'{key}\'').format(sentence=sentence, key=self.key)
-        self.conn.execute(sql)
-        sql = ('UPDATE word_entire '
-               'SET net_detail=\'{sentence}\' '
-               'WHERE word=\'{key}\'').format(sentence=sentence, key=self.key)
-        self.conn.execute(sql)
-        return sentence_list
+#     def parsing_from_xml(self, xml_bytes):
+#         root = ET.fromstring(xml_bytes)
+#         sentence_list = []
+#         for sent in root.findall('sent'):
+#             eng, chn = sent.getchildren()
+#             eng = eng.text.strip()
+#             chn = chn.text.strip()
+#             sentence_list.append((eng, chn))
+#         sentence = ['\n'.join([eng.replace('\'', '\'\''),
+#                                chn.replace('\'', '\'\'')])
+#                     for eng, chn in sentence_list]
+#         sentence = '\r\n'.join(sentence)
+#         sql = ('UPDATE word_mini '
+#                'SET net_detail=\'{sentence}\' '
+#                'WHERE word=\'{key}\'').format(sentence=sentence, key=self.key)
+#         self.conn.execute(sql)
+#         sql = ('UPDATE word_slim '
+#                'SET net_detail=\'{sentence}\' '
+#                'WHERE word=\'{key}\'').format(sentence=sentence, key=self.key)
+#         self.conn.execute(sql)
+#         sql = ('UPDATE word_entire '
+#                'SET net_detail=\'{sentence}\' '
+#                'WHERE word=\'{key}\'').format(sentence=sentence, key=self.key)
+#         self.conn.execute(sql)
+#         return sentence_list
--- app/word/word_parsing.py
+++ app/word/word_parsing.py
@@ -1,79 +1,101 @@
-import re
+# import re
+
+from sql import OperateDB
+from . import WordProto_pb2 as wp
 
 from .. import conn
-from .sql import SelectSQL, UpdateDetail
-from .WordDetailProto_pb2 import (Explain, Relation, Sentence, WordDetail,
-                                  WordList)
 
-select_SQL = SelectSQL(conn)
-update_detail = UpdateDetail(conn)
+sql = OperateDB(conn)
+
 
 class GetWordList(object):
-    def get_word_list(self, request_word, count, is_divide, is_tag_list):
-        word_list = WordList()
-        result_list = select_SQL.select(request_word, mode='list', count=count)
-        if result_list['status']:
-            for result_item in result_list['data']:
-                word_detail = WordDetail()
-                word_detail.word = result_item['word']
-                if is_tag_list:
-                    word_detail.tag_flag.extend(self.get_tag_list(result_item['tag']))
-                else:
-                    word_detail.tag = result_item['tag']
-                if is_divide:
-                    explains_list = self.get_word_chn_explains_in_list(result_item['translation'])
-                    for explain in explains_list:
-                        word_detail.explain.extend([Explain(pos=explain[0], meaning=explain[1])])
-                else:
-                    word_detail.explain.extend([Explain(pos='', meaning=result_item['translation'])])
-                word_list.wordDetail.extend([word_detail])
+    def get_word_list(self, request_word, **kwargs):
+        word_list = wp.WordList()
+        result = sql.select_list(request_word, **kwargs)
+        if result['status']:
+            for data in result['data']:
+                word_list.append(self.get_brief(data))
         return word_list
 
-    def get_tag_list(self, tag_str):
-        if not tag_str:
-            return []
-        tag_list = []
-        tag_list.append(1 if 'zk' in tag_str else 0)
-        tag_list.append(1 if 'gk' in tag_str else 0)
-        tag_list.append(1 if 'ky' in tag_str else 0)
-        tag_list.append(1 if 'cet4' in tag_str else 0)
-        tag_list.append(1 if 'cet6' in tag_str else 0)
-        tag_list.append(1 if 'toefl' in tag_str else 0)
-        tag_list.append(1 if 'ielts' in tag_str else 0)
-        tag_list.append(1 if 'gre' in tag_str else 0)
-        return tag_list
+    def get_brief(self, data):
+        word_brief = wp.WordBrief()
+        word_brief.word_in = data['word']
+        word_brief.word_out = data['word']
 
-    def get_word_chn_explains_in_list(self, word_chn_explains_in_line):
-        l = []
-        for explain in word_chn_explains_in_line.split("\n"):
-            explain_pos_meaning = explain.split(' ', 1)
-            if len(explain_pos_meaning) == 2:
-                l.append(explain.split(' ', 1))
-            else:
-                l.append(['', explain])
-        return l
+        word_lemma = wp.Lemma()
+        if 'lemma' in data:
+            word_lemma.isLemma = False
+            word_lemma.relation = data['lemma']['relation']
+            word_brief.word_out = data['lemma']['word']
+        else:
+            word_lemma.isLemma = True
+        word_brief.Lemma.MergeFrom(word_lemma)
+
+        # phonetic
+        # word_pron = wp.Pronunciation()
+        # if 'pron' in self.data:
+        # pass
+
+        if 'eng_def' in data:
+            eng_defs = data['eng_def']
+            for eng_def in eng_defs.split("\n"):
+                word_eng_def = wp.Definition()
+                pos_meaning = eng_def.split(' ', 1)
+                if len(pos_meaning) == 2:
+                    word_eng_def.pos = pos_meaning[0]
+                    word_eng_def.meaning = pos_meaning[1]
+                else:
+                    word_eng_def.meaning = eng_def
+                word_brief.eng_definitions.append(word_eng_def)
+        if 'chn_def' in data:
+            chn_defs = data['chn_def']
+            for chn_def in chn_defs.split("\n"):
+                word_chn_def = wp.Definition()
+                pos_meaning = chn_def.split(' ', 1)
+                if len(pos_meaning) == 2:
+                    word_chn_def.pos = pos_meaning[0]
+                    word_chn_def.meaning = pos_meaning[1]
+                else:
+                    word_chn_def.meaning = chn_def
+                word_brief.chn_definitions.append(word_chn_def)
+
+        if 'tag' in data:
+            tag_str = data['tag']
+            tag_list = []
+            tag_list.append(1 if 'zk' in tag_str else 0)
+            tag_list.append(1 if 'gk' in tag_str else 0)
+            tag_list.append(1 if 'ky' in tag_str else 0)
+            tag_list.append(1 if 'cet4' in tag_str else 0)
+            tag_list.append(1 if 'cet6' in tag_str else 0)
+            tag_list.append(1 if 'toefl' in tag_str else 0)
+            tag_list.append(1 if 'ielts' in tag_str else 0)
+            tag_list.append(1 if 'gre' in tag_str else 0)
+            word_brief.tags = tag_list
+
+        return word_brief
 
 
-class GetWordDetail(object):
-    def get_word_detail(self, request_word, is_stem):
-        self.request_word = request_word
-        self.actual_query_word = request_word
+class GetWordDetail(GetWordList):
+    def get_word_detail(self, request_word, **kwargs):
+        word_brief = self.get_word_list(request_word, **kwargs)
+        word_detail = WordDetail()
+        word_detail.word_brief = word_brief
+        request_word = word_brief.word_out
+        result = sql.select_detail(request_word)
+        if result['status']:
+            data = result['data']
 
-        self.word_detail = WordDetail()
-        self.word_detail.word = request_word
+            if 'collins' in data:
+                word_detail.collins = data['collins']
 
-        self.result = select_SQL.select(request_word, mode='detail')
-        if self.result['status']:
-            if is_stem:
-                self.find_plain()
-            if self.result['definition']:
-                self.word_detail.eng_explain = self.result['definition']
-            if self.result['collins']:
-                self.word_detail.collins = self.result['collins']
-            if self.result['tag']:
-                self.word_detail.tag = self.result['tag']
-            if self.result['oxford_detail']:
-                sentence_list = self.result['oxford_detail'].split('\r\n')
+            if 'bnc' in data:
+                word_detail.bnc = data['bnc']
+
+            if 'frq' in data:
+                word_detail.frq = data['frq']
+
+            if 'oxford_detail' in data:
+                sentence_list = data['oxford_detail'].split('\r\n')
                 if sentence_list[-1] == '':
                     sentence_list = sentence_list[:-1]
                 split_sentence_list = []
@@ -82,39 +104,33 @@ class GetWordDetail(object):
                     if len(s) == 1:
                         s.append('')
                     split_sentence_list.append(s)
-                sentence_list = [Sentence(source='oxford_detail',
-                                          eng=s[0], chn=s[1])
+                sentence_list = [wp.Sentence(source=1, eng=s[0], chn=s[1])
                                  for s in split_sentence_list]
-                self.word_detail.sentence.extend(sentence_list)
-            if self.result['net_detail']:
-                sentence_list = self.result['net_detail'].split('\r\n')
+                word_detail.sentences.extend(sentence_list)
+
+            if 'net_detail' in data:
+                sentence_list = data['net_detail'].split('\r\n')
                 if sentence_list[-1] == '':
                     sentence_list = sentence_list[:-1]
-                sentence_list = [Sentence(source='net_detail',
-                                          eng=s.split('\n')[0],
-                                          chn=s.split('\n')[1])
+                split_sentence_list = []
+                for s in sentence_list:
+                    s = s.split('\n')
+                    if len(s) == 1:
+                        s.append('')
+                    split_sentence_list.append(s)
+                sentence_list = [wp.Sentence(source=0, eng=s[0], chn=s[1])
                                  for s in sentence_list]
-                self.word_detail.sentence.extend(sentence_list)
-            else:
-                sentence_list = update_detail.fetch_from_iciba(
-                    self.actual_query_word)
-                sentence_list = [Sentence(source='net_detail',
-                                          eng=eng, chn=chn)
-                                 for eng, chn in sentence_list]
-                self.word_detail.sentence.extend(sentence_list)
-        return self.word_detail
+                word_detail.sentence.extend(sentence_list)
+            # iciba
+            # else:
+            #     sentence_list = sql.update_detail(request_word)
+            #     sentence_list = [wp.Sentence(source=0, eng=eng, chn=chn)
+            #                      for eng, chn in sentence_list]
+            #     word_detail.sentence.extend(sentence_list)
 
-    def find_plain(self):
-        relation = Relation()
-        exchange = self.result['exchange']
-        match_0 = re.findall(r"0:[^/]+", exchange)
-        match_1 = re.findall(r"1:[^/]+", exchange)
+            if 'derivative' in data:
+                for item in data['derivative']:
+                    d = wp.Derivative(item[0], item[1])
+                    word_detail.derivatives.append(d)
 
-        if len(match_0) == 1 and len(match_1) == 1:
-            relation.plain_word = match_0[0][2:]
-            relation.relationship = match_1[0][2:]
-            self.result = select_SQL.select(relation.plain_word, mode='detail')
-        else:
-            relation.plain_word = self.request_word
-        self.word_detail.relation.CopyFrom(relation)
-        self.actual_query_word = relation.plain_word
+        return word_detail
