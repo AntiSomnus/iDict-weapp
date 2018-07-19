97a873fc0a0ba9f2157072132ff117d4f8ab7121
taoxinyi <850804004@qq.com>
2018-07-19 13:04:05 +0800

    Add gzip

--- manage.py
+++ manage.py
@@ -1,8 +1,19 @@
 from app import create_app
 from flask_script import Manager
+from flask_compress import Compress
 
 app = create_app()
+app.config['COMPRESS_MIMETYPES'] = [
+    'text/html',
+    'text/css',
+    'text/xml',
+    'application/json',
+    'application/javascript',
+    'application/x-protobuf',
+]
+app.config['COMPRESS_LEVEL'] = 2
 app.debug = True
+Compress(app)
 manager = Manager(app)
 
 if __name__ == '__main__':
